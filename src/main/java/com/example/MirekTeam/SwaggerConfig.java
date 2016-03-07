476c8daae81e47f130d044a53228c3b0cd5c8488
Piotr Uszler <xothas@gmail.com>
2016-03-07 15:54:20 +0100

    ...

--- src/main/java/com/example/MirekTeam/SwaggerConfig.java
+++ src/main/java/com/example/MirekTeam/SwaggerConfig.java
@@ -1,20 +1,15 @@
 package com.example.MirekTeam;
 
-import com.google.common.base.Predicate;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import static com.google.common.base.Predicates.or;
-import static springfox.documentation.builders.PathSelectors.regex;
+
+
+
 /**
  * Created by Shardo on 07.03.2016.
  */
+/**
 @Configuration
+
 @EnableSwagger2
 public class SwaggerConfig {
 
@@ -44,5 +39,5 @@ public class SwaggerConfig {
     }
 }
 
-
+**/
 
